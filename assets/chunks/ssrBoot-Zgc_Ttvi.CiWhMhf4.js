ec24e3ebbbaa0fa4a13d204e7b4a1cc3b9ee75ae
lubojr <lubojr@seznam.cz>
2025-11-04 13:02:07 +0000

    Deploying to gh-pages from @ gtif-ukif/gtif-ukif-client@85fc8f95fe28d9259c6a053b8ae6282c4a2157a4 🚀

---
+++ assets/chunks/ssrBoot-Zgc_Ttvi.CiWhMhf4.js
@@ -0,0 +1,1 @@
+import{D as t,v as e,aq as s,S as n}from"./framework.3XVMDcJ_.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
