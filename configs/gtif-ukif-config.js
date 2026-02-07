0b676ea4e5095e3e9b96870ff689158c90ac17df
lubojr <lubojr@seznam.cz>
2026-02-07 10:14:22 +0000

    Deploying to gh-pages from @ gtif-ukif/gtif-ukif-client@2dc0e70c5677b86bb89b4e007bcaccc326bce75a 🚀

--- configs/gtif-ukif-config.js
+++ configs/gtif-ukif-config.js
@@ -158,7 +158,7 @@ export default {
               ? {
                   id: "Datepicker",
                   type: "internal",
-                  layout: { x: 4, y: 6, w: 4, h: 6 },
+                  layout: { x: 4, y: 3, w: 4, h: 9 },
                   title: "Date",
                   widget: {
                     name: "EodashDatePicker",
@@ -375,7 +375,7 @@ export default {
               ? {
                   id: "Datepicker",
                   type: "internal",
-                  layout: { x: 4, y: 6, w: 4, h: 6 },
+                  layout: { x: 4, y: 3, w: 4, h: 9 },
                   title: "Date",
                   widget: {
                     name: "EodashDatePicker",
